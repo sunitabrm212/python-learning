8b16be8ba7e8114a686a84f1b110d1f79396fcb2
sunita baram <sunitabaram489@gmail.com>
2024-02-25 18:23:04 +0545

    array

---
+++ array.py
@@ -0,0 +1,15 @@
+#array
+# from array import*
+# arr=array('i',[1,2,3,4,5])
+# print(type(arr))
+# arr.append(7)
+# print(arr)
+
+array asking from user
+
+n=int(input("Enter the size of array: "))
+arr=[]
+for i in range(n):
+    x=int(input("Enter the next number: "))
+    arr.append(x)
+print(arr)
