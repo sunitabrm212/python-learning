76aa1331b14166e5f837d34c5c1c2d12cc6a5fdd
sunita baram <sunitabaram489@gmail.com>
2024-02-25 15:50:31 +0545

    methods and name variable in python

---
+++ calc.py
@@ -0,0 +1,17 @@
+# def add(a,b):
+#     return a+b
+# def sub(a,b):
+#     return a-b
+# def mul(a,b):
+#     return a*b
+# def div(a,b):
+#     return a/b
+
+# __name__ variable
+
+# import demo
+# print(__name__)
+# so the main function will be appeared only if it is called or else content in demo will not be displayed in calc
+import demo
+print("It's time to make tea")
+
---
+++ demo.py
@@ -0,0 +1,13 @@
+# import calc
+# a=9
+# b=3
+# c=calc.mul(a,b)
+# print(c)
+
+# print("Hello"+__name__)
+def main():
+    print ("Hello")
+    print("Karina Musi!")
+
+if __name__ == "__main__":
+    main()
