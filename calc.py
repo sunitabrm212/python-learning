ce27c5eccb48133a49ac41485d6a78eacbe00b3b
sunita baram <sunitabaram489@gmail.com>
2024-02-25 16:57:04 +0545

    __name__ variable part 2

--- calc.py
+++ calc.py
@@ -12,6 +12,19 @@
 # import demo
 # print(__name__)
 # so the main function will be appeared only if it is called or else content in demo will not be displayed in calc
-import demo
-print("It's time to make tea")
+# import demo
+# print("It's time to make tea")
 
+
+#name variable in main function
+from demo import add
+def fun1():
+    add()
+    print("fun 1")
+def fun2():
+    print("fun 2")
+
+def main():
+    fun1()
+    fun2()
+main()
--- demo.py
+++ demo.py
@@ -5,9 +5,24 @@
 # print(c)
 
 # print("Hello"+__name__)
-def main():
-    print ("Hello")
-    print("Karina Musi!")
+# def main():
+#     print ("Hello")
+#     print("Karina Musi!")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+#name variable in main function
+
+def add():
+    print("result 1",__name__)
+
+def sub():
+    print("resutl 2")
+
+def main():
+    add()
+    sub()
+
+if __name__=="__main__": #if name value is equla to main then func will be called.
+     main()
